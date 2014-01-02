21b93e6e9e2fd2e019edcb42c3b0c02245aa8784
Michael <michael@michael-VirtualBox.(none)>
2014-01-02 23:28:32 +0000

    Update all files

---
+++ README.md
@@ -0,0 +1,7 @@
+
+Practice App to try out Git and Jenkins CI
+
+[*Git and Jenkins practice app*]
+by [Michael Griffiths]
+
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,16 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+<<<<<<< HEAD
+MichaelApp::Application.config.secret_key_base = '115adac761d947b53cda685a86e2b4d5b0d7f025e125fe3f8c9391d571e56bb40fbacf5f393b14e7339ec282d456f4ef0bf4b907226ba10d79f435d156749f04'
+=======
+MichaelApp::Application.config.secret_key_base = '46ddd5b5b7bc97d6b2219ebdaf3a890a41626959cdf19fd7d9a6db4002d8d6ef1d62ef9796e43edab360790f281889d693f003f3fb40de29b5a1d3ec54237567'
+>>>>>>> d1fddd225d99e55751cc53eb577baa42280cda53
