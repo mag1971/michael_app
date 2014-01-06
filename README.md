a598db44035fd4fe9c95155398a9217baf59b75b
Michael <michael@michael-VirtualBox.(none)>
2014-01-06 21:08:25 +0000

    deleting files

--- README.md
+++
@@ -1,7 +0,0 @@
-#Practice App to try out Git and Jenkins CI
-
-**Git and Jenkins practice app**
-
-by ***Michael Griffiths***
-
-
--- config/initializers/session_store.rb
+++
@@ -1,3 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-MichaelApp::Application.config.session_store :cookie_store, key: '_michael_app_session'
